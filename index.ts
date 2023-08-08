5733cf42428b170eecacafbafacfd848a04d9da1
hataehyeok <hth021002@postech.ac.kr>
2023-08-08 22:12:01 +0900

    Refactoring Box class

--- index.ts
+++ index.ts
@@ -193,12 +193,16 @@ class Box implements Tile {
   moveHorizontal(dx: number) { this.falling.moveHorizontal(this, dx); }
   moveVertical(dy: number) { }
   update(x: number, y: number) { 
+    this.falling = map[y + 1][x].isAir()
+      ? this.falling = new FallingBox()
+      : this.falling = new RestingBox();
+    this.drop(this, x, y);
+  }
+  
+  private drop(tile: Tile, x: number, y: number) {
     if (map[y + 1][x].isAir()) {
-      this.falling = new FallingBox()
-      map[y + 1][x] = this;
+      map[y + 1][x] = tile;
       map[y][x] = new Air();
-    } else if (this.falling.isFalling()) {
-      this.falling = new RestingBox()
     }
   }
 }
